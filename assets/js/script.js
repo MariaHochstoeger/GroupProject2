a18c516483f703d319aab50289369efeb22c2bce
Maria Hochstoeger <m_hochstoeger@hotmail.com>
2023-09-06 15:11:33 +0000

    wrap generate random number into a function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 // generate a random number between 1-100
-let randomNumber = Math.floor((Math.random() * 100) + 1); //taken from https://www.w3schools.com/jsref/jsref_random.asp
+function genRandomNumber() {
+    let randomNumber = Math.floor((Math.random() * 100) + 1); //taken from https://www.w3schools.com/jsref/jsref_random.asp
+    return randomNumber;
+}
 
 //compare randomNumber against userNumber
 let userNumber = document.getElementById('number').value; //do I have to put .value or not?
