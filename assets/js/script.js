66189d007f58af0f53434b927a70ab9a393f2d7c
Mohamed Abdel Ghany <mufasa1611@gmail.com>
2023-09-06 14:43:02 +0000

    add basic structure

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+     // access the html elements
+
+    // Generate a random number between 1 and 100 
+    
+    // Get the users guess number
+
+    // Check if the user's guess is a valid number
+
+    // Check if the user's guess is correct
+
+    // Calculate the percentage difference ----(optional)
+
+    // Send feedback message based on answer 
+    
+    // Clear the input field
+
+    // Decrement chances ----(optional)
+
+    // Check if chances are exhausted ----(optional)
+
+    // Display a message with options to try again or quit
+
+    // Reset the game 
