9ad0c5c7534f37e0b2ecef67fb61caa52460ff0e
Mohamed Abdel Ghany <mufasa1611@gmail.com>
2023-09-07 00:07:47 +0000

    add js code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,44 +1,41 @@
-
 // generate a random number between 1-100
 function genRandomNumber() {
-    let randomNumber = Math.floor((Math.random() * 100) + 1); //taken from https://www.w3schools.com/jsref/jsref_random.asp
+    let randomNumber = Math.floor((Math.random() * 10) + 1); //source: https://www.w3schools.com/jsref/jsref_random.asp
     return randomNumber;
 }
 
-//compare randomNumber against userNumber
-let userNumber = document.getElementById('number').value; //do I have to put .value or not?
-if (userNumber === randomNumber) {
-    //Mohamed to write code for success message
-} else if (userNumber < randomNumber) {
-    //Mohamed to write code for too low message
-} else {
-    //Mohamed to write code for too high message
-};
+let randomNumber = genRandomNumber(); // var to hold the RND#
+
+//access and load elements & compare randomNumber against userNumber
+
+document.addEventListener("DOMContentLoaded", function () {
+    let userNumber = document.getElementById('number'); //.value; //do I have to put .value or not?
+    let message = document.getElementsByClassName("feedback-message")[0];
+    let button = document.querySelector('input[value="Guess"]');
+
+    if (button) {
+        button.addEventListener("click", function (event) {
+            event.preventDefault(); // source: (https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault)
+
+           
+            // get the users guess number
+            let guess = parseInt(userNumber.value);
+
+            
+            // to check users guess and compare it with RND#
+            if (guess === randomNumber) {
+                message.textContent = "Congratulations! You guessed it right!";
+                popup();
 
 
+            } else if (guess < randomNumber) {
 
+                message.textContent = "That was too low. Guess again!";
+            } else {
+                message.textContent = "That was too high. Guess again!";
+            }
+        });
+    }
+});
 
-     // access the html elements
-
-   
-    
-    // Get the users guess number
-
-    // Check if the user's guess is a valid number
-
-    // Check if the user's guess is correct
-
-    // Calculate the percentage difference ----(optional)
-
-    // Send feedback message based on answer 
-    
-    // Clear the input field
-
-    // Decrement chances ----(optional)
-
-    // Check if chances are exhausted ----(optional)
-
-    // Display a message with options to try again or quit
-
-    // Reset the game 
 
