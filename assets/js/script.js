a999d674d33a18cbb8616afb592fd5bee14c130e
Mohamed Abdel Ghany <mufasa1611@gmail.com>
2023-09-08 09:01:12 +0000

    add (alt),remove(pattern), reposition the js var and change button color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,20 @@
+    let userNumber = document.getElementById('number'); 
+    let message = document.getElementsByClassName("feedback-message")[0];
+    let button = document.querySelector('input[value="Guess"]');
+    let randomNumber = genRandomNumber(); // var to hold the RND#
+
 // generate a random number between 1-100
 function genRandomNumber() {                                  //TODO: change back to 100
     let randomNumber = Math.floor((Math.random() * 10) + 1); //source: https://www.w3schools.com/jsref/jsref_random.asp
     return randomNumber;
 }
 
-let randomNumber = genRandomNumber(); // var to hold the RND#
-
 //access and load elements & compare randomNumber against userNumber
-
 document.addEventListener("DOMContentLoaded", function () {
-    let userNumber = document.getElementById('number'); //.value; //do I have to put .value or not?
-    let message = document.getElementsByClassName("feedback-message")[0];
-    let button = document.querySelector('input[value="Guess"]');
-
+    
     if (button) {
         button.addEventListener("click", function (event) {
             event.preventDefault(); // source: (https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault)
-
            
             // get the users guess number
               let guess = parseInt(userNumber.value);
@@ -63,6 +61,7 @@ function popup() {
     } else {
         alert("Goodbye, see you soon!");
         document.getElementById('number').disabled = true;
+        document.getElementsByClassName("feedback-message")[0].textContent = "";
     }
 }
 
